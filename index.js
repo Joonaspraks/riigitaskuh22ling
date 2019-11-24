2b057ab274b0a01b4bf794ea75aef23b303615f2
Joonas Praks <joonaspraks@gmail.com>
2019-11-24 22:46:55 +0200

    testing logging

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-var http = require("http");
+const http = require("http");
+const opts = {
+  errorEventName:'error',
+      logDirectory:'logs', // NOTE: folder must exist and be writable...
+      fileNamePattern:'<DATE>.log',
+      dateFormat:'YYYY.MM.DD'
+};
+const log = require('simple-node-logger').createRollingFileLogger( opts );
 
 const subscriber = require("./subscriber.js");
 const endPointHandler = require("./endPointHandler.js");
@@ -7,6 +14,7 @@ subscriber.renewSubscriptions();
 
 http
   .createServer(function(request, response) {
+    log.info('Server called at ', new Date().toJSON());
     endPointHandler.parse(request, response);
   })
   .listen(process.env.PORT || 80);
