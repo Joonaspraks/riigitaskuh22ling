a1e0bb2d283fb69962905ec68b91cb19fd74d2d2
Joonas Praks <joonaspraks@gmail.com>
2019-11-24 23:36:50 +0200

    test log

---
+++ logger.js
@@ -0,0 +1,10 @@
+const opts = {
+    logDirectory:'./logs', // NOTE: folder must exist and be writable...
+    fileNamePattern:'<DATE>.log',
+    dateFormat:'YYYY.MM.DD'
+};
+const log = require("simple-node-logger").createRollingFileLogger(opts);
+
+module.exports = {
+  log: log
+};
