e0c4e34604008ec79c703e94f2367e31da03957e
Joonas Praks <joonaspraks@gmail.com>
2019-12-01 15:25:40 +0200

    Testing ffmpeg

--- src/soundFixer.js
+++ src/soundFixer.js
@@ -3,35 +3,38 @@ const log = require("./logger.js").log;
 
 function extractAndEditAudio(ytStream, title /* description */) {
   let description = "";
-  ytStream.on("info", info => {
-    description = info.description;
-  })
-  return (
-    ffmpeg(ytStream)
-      //noise removal
-      //detect general audio level to cut silence
-      //note that questioneers mic sound can be lower than the ministers
-      .audioFilters(
-        "silenceremove=" +
-        /*             'start_periods=0:'+ //trim until 1 non-silence
-               'start_duration=0:'+ //atleast that amount of seconds to be a 'silence'
-               'start_threshold=0:'+ //what dB constitutes a 'silence' */
+  ytStream
+    .on("info", info => {
+      description = info.description;
+    })
+    .end(() => {
+      return (
+        ffmpeg(ytStream)
+          //noise removal
+          //detect general audio level to cut silence
+          //note that questioneers mic sound can be lower than the ministers
+          .audioFilters(
+            "silenceremove=" +
+            /*             'start_periods=0:'+ //trim until 1 non-silence
+                 'start_duration=0:'+ //atleast that amount of seconds to be a 'silence'
+                 'start_threshold=0:'+ //what dB constitutes a 'silence' */
 
-        "stop_periods=-1:" + //negative value means that silencing will occur in the middle of the file, '1' means that 'trim until 1 non-silence'
-        "stop_duration=3:" + //atleast that amount of seconds to be a 'silence'
-          "stop_threshold=-35dB",
+            "stop_periods=-1:" + //negative value means that silencing will occur in the middle of the file, '1' means that 'trim until 1 non-silence'
+            "stop_duration=3:" + //atleast that amount of seconds to be a 'silence'
+              "stop_threshold=-35dB",
 
-        //'earwax'+
-        //'loudnorm',
-        "dynaudnorm"
-      ) //what dB constitutes a 'silence'
+            //'earwax'+
+            //'loudnorm',
+            "dynaudnorm"
+          ) //what dB constitutes a 'silence'
 
-      .on("progress", progress => log.info(JSON.stringify(progress)))
-      //.save('earwaxIstung2.mp3');
-      .outputOption(`-metadata title=${description}`) 
-      .save("./storedAudio/" + title + ".mp3")
-  );
-  //.save('loudnormIstung2.mp3');
+          .on("progress", progress => log.info(JSON.stringify(progress)))
+          //.save('earwaxIstung2.mp3');
+          .outputOption(`-metadata title=${description}`)
+          .save("./storedAudio/" + title + ".mp3")
+      );
+      //.save('loudnormIstung2.mp3');
+    });
 }
 
 module.exports = { extractAndEditAudio: extractAndEditAudio };
