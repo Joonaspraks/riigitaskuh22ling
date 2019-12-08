fe6c65e58f2db6d93f23949f3857d3e274fe1bd3
Joonas Praks <joonaspraks@gmail.com>
2019-12-08 18:54:35 +0200

    trying to add metadata to a file

--- src/endPointHandler.js
+++ src/endPointHandler.js
@@ -65,7 +65,6 @@ function downloadAudio(id, title) {
             currentCredentials
           );
           localFileManager.removeOldContent();
-          localFileManager.createRSS();
         });
     }
   });
